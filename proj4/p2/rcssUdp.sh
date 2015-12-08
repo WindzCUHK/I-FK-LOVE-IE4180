f116271c419b806a17d218f4f3bd7ac93a6013da
unknown <windz.fan@Windz-PC.2gotech.com>
2015-12-08 10:07:58 +0800

    clean up duplication

--- proj4/p2/rcssUdp.sh
+++
@@ -1,1 +0,0 @@
-./b.out -response -stat 500 -rhost 127.0.0.1 -rport 4180 -lport 4181 -proto udp -pktsize 16 -pktnum 10 -sbufsize 2048 -rbufsize 4096
--- proj4/p3/linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-g++ -Wall -Wno-write-strings -std=c++11 -pthread *.cpp -o NetProbe
-
-cmd="./NetProbe s 41801 o > out.txt"
-echo 'Starting: ' $cmd
-$cmd
-echo ''
